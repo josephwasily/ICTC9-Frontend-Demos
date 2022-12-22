894cf03269ea1b8fa4118960e78afc4f2fb477b0
Joseph Botros <jwasily@hotmail.com>
2022-12-22 15:37:09 +0300

    add changes

--- Intro to Angular/forms/src/app/students/create/create.component.ts
+++ Intro to Angular/forms/src/app/students/create/create.component.ts
@@ -1,39 +1,93 @@
+import { HttpClient } from '@angular/common/http';
 import { StudentsAsyncService } from './../students-async.service';
 import { StudentsService } from './../students.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Student } from '../student';
 import { Router } from '@angular/router';
-import {interval, filter, map} from 'rxjs'
+import {interval, filter, map, Observable, from, fromEventPattern, fromEvent, Subscription, tap, switchMap, of} from 'rxjs'
+import { throwToolbarMixedModesError } from '@angular/material/toolbar';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
-export class CreateComponent implements OnInit {
+export class CreateComponent implements OnInit, OnDestroy {
   student: Student = {
     firstName: ''
   }
   students:Student[] = [];
-  constructor(private studentsService: StudentsAsyncService, private router: Router){
+  students$? : Observable<Student[]>;
+  sub?: Subscription;
+  constructor(private studentsService: StudentsAsyncService, private router: Router, private http: HttpClient){
 
   }
-  ngOnInit(): void {
-    this.studentsService.getStudents().subscribe((response)=> {
-      this.students = response;
-    });
+  ngOnDestroy(): void {
 
-    interval(1000)
-    .pipe(
-      filter((value)=> value % 2 == 0), //observable
-      map((value)=> 'the number is '+value),
-
-    ).subscribe((value)=> { 
-       
-          console.log(value);
-       
-    })  
+    this.sub?.unsubscribe();
     
+  }
+  ngOnInit(): void {
+   this.studentsService.getStudents().subscribe((students)=> {
+    this.students = students;
+   });
+
+    let obs$ = new Observable((subscriber)=> {
+        subscriber.next('first event');
+        setTimeout(()=> {
+            subscriber.next('second event after period of time');
+            //subscriber.error();
+
+            //logic 
+        }, 1000);
+       
+    });
+    let observer = {
+      next: (val: any)=> console.log(val),
+      error: (error: any)=> console.log(error, 'from the observer/subscriber'),
+      complete: ()=> console.log('the observable has completed')
+    }
+    // obs$.subscribe((value)=>  {
+    //     console.log(value);
+    // });
+    
+    let obs2$ = from([{isUrgent: true, title: 'catastrophe'}, {isUrgent: false, title: 'regular event'}])
+    .pipe(
+      
+      filter((value)=> value.isUrgent),
+      map((value)=> value.title),
+
+    );
+    obs2$.subscribe((value)=> alert(value));
+
+    // obs2$.subscribe((value)=>{
+    //   if(value.isUrgent){
+    //     alert(value.title);
+    //   }
+    // })
+
+    let obs3$ =  fromEvent(document, 'click');
+    this.sub =  obs3$.subscribe((value)=> {
+      console.log(value);
+    });
+    // setTimeout(()=> this.sub.unsubscribe(), 5000);
+
+    let nationality$ = this.http.get<Nationality>('https://api.nationalize.io?name=nathaniel')
+    .pipe(
+      map((obj)=> {
+        let codes = '';
+        obj.country.forEach(element => {
+            codes+= element.country_id+','
+        });
+        return codes;
+      }),
+      switchMap((value)=> {
+          return this.http.get<RestCountry[]>('https://restcountries.com/v3.1/alpha?codes='+value)
+      }),
+      map((value)=> value.map((x)=>  {x.name.common}))
+    ).subscribe(value=> console.log(value));
   //this.students =
+
+
   }
   logForm(f: any){
     console.log(f);
@@ -52,3 +106,25 @@ export class CreateComponent implements OnInit {
     //navigate
   }
 }
+
+
+export interface Nationality {
+  country: Country[];
+  name:    string;
+}
+
+export interface Country {
+  country_id:  string;
+  probability: number;
+}
+
+
+export interface RestCountry {
+  name  :  {
+    common: string
+  },
+  flags: {
+    png: string
+  }
+}
+
